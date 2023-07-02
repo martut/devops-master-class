48acf92c23a9bad79e23666bcd14c8ff1e320e8c
Marcin Wirzeski <40631628+martut@users.noreply.github.com>
2023-07-02 22:14:12 +0200

    lists

---
+++ terraform/terraform03/main.tf
@@ -0,0 +1,13 @@
+variable "names" {
+  default = ["marcin", "ravi", "siva"]
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_user" "my_iam_users" {
+#  count = length(var.names)
+#  name  = var.names[count.index]
+  for_each = toset(var.names)
+  name = each.value
+}
