612a60c847e3e816f3697602431ff36ee48565e4
Marcin Wirzeski <40631628+martut@users.noreply.github.com>
2023-10-13 11:52:46 +0200

    clasic load balancer

--- terraform/terraform06/main.tf
+++ terraform/terraform06/main.tf
@@ -1,46 +1,18 @@
-variable "aws_key_pair" {
-  default = "~/aws/aws_keys/default-ec2.pem"
-}
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_default_vpc" "default" {
-  
-}
-
-data "aws_subnets" "default_subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [aws_default_vpc.default.id]
-  }
-}
-
-data "aws_ami" "aws-linux-2-latest" {
-  most_recent = true
-  owners = ["amazon"]
-  filter {
-    name = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm*"]
-  }
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-}
-
-data "aws_ami_ids" "aws-linux-2-latest_ids" {
-  owners = ["amazon"]
 }
 
 //http server -> SG
 // SG -> 80 tcp, ssh 22 tcp, CIDR ["0.0.0.0/0"]
 
 resource "aws_security_group" "http_server_sg" {
-  name   = "http_server_sg"
+  name = "http_server_sg"
   //vpc_id = "vpc-074c4e3e073ac4874"
   vpc_id = aws_default_vpc.default.id
-  
+
   ingress {
     from_port   = 80
     to_port     = 80
@@ -67,15 +39,62 @@ resource "aws_security_group" "http_server_sg" {
   }
 }
 
-resource "aws_instance" "http_server" {
+resource "aws_security_group" "elb_sg" {
+  name = "elb_sg"
+  //vpc_id = "vpc-074c4e3e073ac4874"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_elb" "elb" {
+  name = "elb"
+  subnets = toset(data.aws_subnets.default_subnets.ids)
+  security_groups = [aws_security_group.elb_sg.id]
+  instances = values(aws_instance.http_servers).*.id
+  
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+}
+
+resource "aws_instance" "http_servers" {
   //ami                    = "ami-06b09bfacae1453cb"
   ami                    = data.aws_ami.aws-linux-2-latest.id
   key_name               = "default-ec2"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.http_server_sg.id]
   //subnet_id              = "subnet-0352be171177c9e8c"
-  subnet_id              = data.aws_subnets.default_subnets.ids[0]
-  
+
+  for_each  = toset(data.aws_subnets.default_subnets.ids)
+  subnet_id = each.value
+
+  tags = {
+    name : "http_servers_${each.value}"
+  }
+
   connection {
     type        = "ssh"
     host        = self.public_ip
--- terraform/terraform06/outputs.tf
+++ terraform/terraform06/outputs.tf
@@ -3,5 +3,9 @@ output "aws_security_group_server_details" {
 }
 
 output "http_server_public_dns" {
-  value = aws_instance.http_server.public_dns
+  value = values(aws_instance.http_servers).*.id
+}
+
+output "elb_public_dns" {
+  value = aws_elb.elb
 }
